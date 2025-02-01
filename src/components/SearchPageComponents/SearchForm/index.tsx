77307577d61d0b4f45afcb0a58288c7d41707644
Anano Topuria <anano.topuriaa@gmail.com>
2025-02-01 19:13:31 +0400

    feat: implement live search and store query in localStorage on submit

--- src/components/SearchPageComponents/SearchForm/index.tsx
+++ src/components/SearchPageComponents/SearchForm/index.tsx
@@ -3,16 +3,27 @@ import { SearchFormProps } from "./types";
 
 function SearchForm({ setSearchTerm }: SearchFormProps) {
   const [searchValue, setSearchValue] = useState<string>("");
-
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newValue = event.target.value;
     setSearchValue(newValue);
     setSearchTerm(newValue);
   }
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (searchValue.trim() === "") return;
+    const storedHistory = JSON.parse(
+      localStorage.getItem("searchHistory") || "[]",
+    );
+    if (!storedHistory.includes(searchValue)) {
+      storedHistory.push(searchValue);
+      localStorage.setItem("searchHistory", JSON.stringify(storedHistory));
+    }
+    setSearchValue("");
+  }
 
   return (
-    <section className="bg-[white] flex justify-center">
-      <form>
+    <section className="bg-white flex justify-center">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           className="text-center w-[20rem]"
